dec54e67426b7b245ef12dbecefe82a4f53a1766
viacognitiva <renato.filho@vbofficeware.com.br>
2018-05-09 10:58:20 -0300

    ajustes

--- app/core/chat/chatController.js
+++ app/core/chat/chatController.js
@@ -257,7 +257,6 @@
 
                $http.get('/api/showSound').then(
                    function(response){
-                       console.log('showSound: ' + response.data.retorno);
                        if(response.data.retorno == 'true'){
                            vm.showSom = true;
                        }else{
@@ -272,7 +271,6 @@
 
                $http.get('/api/showLog').then(
                    function(response){
-                       console.log('showLog: ' + response.data.retorno);
                        if(response.data.retorno == 'true'){
                            vm.showLog = true;
                        }else{
